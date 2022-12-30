6312f31cac607b37966dad631702c5d6aa14efbf
Precious Olusola <preciousolusola16@gmail.com>
2022-12-30 15:31:15 +0100

    Added production support

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const crypto = require('crypto');
 const {UserDb, defaultTodo, TasksDb} = require('./db');
+
 const io = require('socket.io')(3000, {
     cors:{
-        origin: ['http://127.0.0.1:5173',]
+        origin: ['http://127.0.0.1:5173', process.env.clientUrl]
     }
 })
 
+console.log(['http://127.0.0.1:5173', process.env.clientUrl])
 /*
     Events
     1) create_account: username, email, password
